0af98861f489ca449e8493ac86927d952b93779b
Artem <artem@infoprofile.ru>
2023-10-19 11:32:13 +0300

    add models card, module and topic

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,13 +1,39 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
-from .models import CustomUser
+from .models import CustomUser, Topic, Module, Card
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('pk', 'email', 'username')
-    list_display_links = ('pk', 'email', 'username')
+    list_display = ("pk", "email", "username", "first_name", "last_name")
+    list_display_links = ("pk", "email", "username", "first_name", "last_name")
+    search_fields = ("email", "username")
 
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "author", "created_at", "updated_at")
+    list_display_links = ("pk", "title", "author", "created_at", "updated_at")
+    search_fields = ("title", "author__username", "description")
+    list_filter = ("author__username", "created_at")
+    date_hierarchy = "created_at"
+    ordering = ["-created_at"]
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "author", "created_at", "updated_at")
+    list_display_links = ("pk", "title", "author", "created_at", "updated_at")
+    search_fields = ("title", "author__username", "description")
+    list_filter = ("author__username", "created_at")
+    date_hierarchy = "created_at"
+    ordering = ["-created_at"]
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ("pk", "front_text", "back_text", "module", "created_at", "updated_at")
+    list_display_links = ("pk", "front_text", "back_text", "module", "created_at", "updated_at")
+    search_fields = ("title", "module__title", "description")
+    list_filter = ("module__title", "created_at")
--- src/quiz/models.py
+++ src/quiz/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -8,3 +9,37 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Topic(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="topics")
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    modules = models.ManyToManyField(to="Module", related_name="topics")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="modules")
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Card(models.Model):
+    module = models.ForeignKey(to="Module", on_delete=models.CASCADE, related_name="cards")
+    front_text = models.TextField()
+    back_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.front_text} - {self.back_text[:30]}..."
