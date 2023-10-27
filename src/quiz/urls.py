f54ba0ec7c2bed2becc75629568899eff8f7d832
Gleb <quanuhs@yandex.ru>
2023-10-27 22:38:36 +0300

    update

--- src/quiz/urls.py
+++ src/quiz/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.main_page),
-    path("module/<id>", views.module_page)
+    path("module/all", views.modules_page, name="modules"),
+    path("module/<id>", views.module_page, name="module"),
+    path("module/edit/<id>", views.module_page, name="edit_module"),
+    path("author/<id>", views.author_page, name="author"),
+    path("login", views.login_page, name="login")
 ]
--- src/quiz/views.py
+++ src/quiz/views.py
@@ -3,13 +3,24 @@ from .models import Module, Topic
 
 
 def main_page(request):
-    modules = Module.objects.all()
-    return render(request, "topic.html", {"modules": modules})
+    return render(request, "main.html")
 
 def topic_page(request, id):
     topic = Topic.objects.all()
     return render(request, "topic.html", {"topic": topic})
 
 def module_page(request, id):
+    modules = Module.objects.all()
     module = Module.objects.filter(id=id).first()
     return render(request, "module.html", {"module": module})
+
+def modules_page(request):
+    modules = Module.objects.all()
+    return render(request, "topic.html", {"modules": modules})
+
+def author_page(request, id):
+    modules = Module.objects.all()
+    return render(request, "topic.html", {"modules": modules})
+
+def login_page(request):
+    return render(request, "login.html")
